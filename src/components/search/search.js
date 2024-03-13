5b378767371dcb6480544681fec5ff73d2c3e1f4
m.kotsurenko <max.v91@yandex.ru>
2024-03-13 17:29:11 +0300

    компонент поиска

---
+++ src/components/search/search.js
@@ -0,0 +1,32 @@
+import { DivComponent } from "../../common/div-component.js";
+import './search.css';
+
+export class Search extends DivComponent
+{
+    constructor(state)
+    {
+        super();
+        this.state = state;
+    }
+
+    render()
+    {
+        this.el.classList.add('search');
+        this.el.innerHTML =
+            `
+            <div class="search__wrapper">
+                <input 
+                    type="text" 
+                    placeholder="Найти книгу или автора..."
+                    class="search__input"
+                    value="${this.state.searchQuery ? this.state.searchQuery : ""}"    
+                />
+                <img src="/static/search.svg" alt="search" />
+            </div>
+            <button aria-label="search">
+                <img src="/static/search-white.svg" alt="search">
+            </button>
+            `;
+        return this.el;
+    }
+}
